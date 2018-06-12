bf5ad30f0c424eb8e5d7b7d30f09676a3e3b1507
Joe Jiang <happiness9721@gmail.com>
2018-06-12 12:43:53 +0800

    add .stCommitMsg

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 # git
 ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
+ln -s ~/.dotfiles/git/.stCommitMsg ~/.stCommitMsg
 
 # vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
@@ -10,6 +11,9 @@ ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
 
 # etc
 ln -s ~/.dotfiles/etc/gemrc ~/.gemrc
+
+# zsh
+cp ~/.dotfiles/zsh/zpreztorc ~/.zprezto/runcoms/zpreztorc
 
 # for config files
 # ln -s ~/.dotfiles/config/gitconfig ~/.gitconfig
