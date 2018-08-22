6e2e8960e9c8c692fc6405a234fd67a0e86a8588
Joe Jiang <happiness9721@gmail.com>
2018-08-22 18:54:48 +0800

    add tigrc

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ ln -s ~/.dotfiles/git/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/git/gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/git/.stCommitMsg ~/.stCommitMsg
 
+# tig
+ln -s ~/.dotfiles/tig/tigrc ~/.tigrc
+
 # vim
 ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
 
